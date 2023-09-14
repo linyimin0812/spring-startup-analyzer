771e02cf903f8435f592ddcda3dcc53bcfe12596
linyimin <linyimin520812@gmail.com>
2023-09-14 13:34:28 +0000

    testcase: add ApplicationRunMonitor test case

---
+++ spring-profiler-core/src/test/java/io/github/linyimin0812/profiler/core/monitor/ApplicationRunMonitorTest.java
@@ -0,0 +1,60 @@
+package io.github.linyimin0812.profiler.core.monitor;
+
+import io.github.linyimin0812.profiler.api.event.AtEnterEvent;
+import io.github.linyimin0812.profiler.api.event.Event;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author linyimin
+ **/
+class ApplicationRunMonitorTest {
+
+    private final ApplicationRunMonitor applicationRunMonitor = new ApplicationRunMonitor();
+
+    @Test
+    void filter() {
+        Assertions.assertTrue(applicationRunMonitor.filter("org.springframework.boot.SpringApplication"));
+        Assertions.assertFalse(applicationRunMonitor.filter("org.springframework.boot.ApplicationContext"));
+    }
+
+    @Test
+    void testFilter() {
+
+        String methodName = "filter";
+
+        String[] methodTypes = null;
+
+        Assertions.assertFalse(applicationRunMonitor.filter(methodName, methodTypes));
+
+        methodTypes = new String[] {"java.lang.Object[]", "java.lang.String[]"};
+
+        Assertions.assertFalse(applicationRunMonitor.filter(methodName, methodTypes));
+
+        methodName = "run";
+        Assertions.assertTrue(applicationRunMonitor.filter(methodName, methodTypes));
+
+        methodTypes = new String[] {"java.lang.Class[]", "java.lang.String[]"};
+
+        Assertions.assertTrue(applicationRunMonitor.filter(methodName, methodTypes));
+
+    }
+
+    @Test
+    void onEvent() {
+        AtEnterEvent event = new AtEnterEvent(0L, 0L, null, null, null, null, null);
+        applicationRunMonitor.onEvent(event);
+        Assertions.assertTrue(applicationRunMonitor.filter("org.springframework.boot.SpringApplication"));
+    }
+
+    @Test
+    void listen() {
+
+        List<Event.Type> events = applicationRunMonitor.listen();
+        Assertions.assertEquals(events.size(), 1);
+        Assertions.assertEquals(Event.Type.AT_EXIT, events.get(0));
+    }
+
+}
