364d86e28d0ff0a85a0a477f4b2a46d7ef95e430
linyimin0812 <linyimin520812@gmail.com>
2023-12-09 19:31:10 +0800

    test: refactor unit test using spock

--- spring-profiler-common/src/test/java/io/github/linyimin0812/profiler/common/ui/BeanInitResultSpec.groovy
+++ spring-profiler-common/src/test/java/io/github/linyimin0812/profiler/common/ui/BeanInitResultSpec.groovy
@@ -1,6 +1,95 @@
 package io.github.linyimin0812.profiler.common.ui
 
+import spock.lang.Specification
+
 /**
  * @author linyimin
- **/class BeanInitResultSpec {
+ * */
+class BeanInitResultSpec extends Specification {
+
+    def "test getId"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        then:
+        beanInitResult.getId() >= 1000
+    }
+
+    def "test getParentId"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        BeanInitResult childBeanInitResult = new BeanInitResult("child")
+        beanInitResult.addChild(childBeanInitResult)
+
+        then:
+        childBeanInitResult.getParentId() == beanInitResult.getId()
+    }
+
+    def "test getName"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+
+        then:
+        "test" == beanInitResult.getName()
+    }
+
+
+    def "test getDuration"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        beanInitResult.duration()
+
+        then:
+        beanInitResult.getDuration() >= 0
+    }
+
+    def "test getActualDuration"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        BeanInitResult childBeanInitResult = new BeanInitResult("child")
+        beanInitResult.addChild(childBeanInitResult)
+        // child finish first
+        childBeanInitResult.duration()
+        beanInitResult.duration()
+
+        then:
+        beanInitResult.getActualDuration() < 10
+    }
+
+    def "test duration"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        BeanInitResult childBeanInitResult = new BeanInitResult("child")
+        beanInitResult.addChild(childBeanInitResult)
+        // child finish first
+        childBeanInitResult.duration()
+        beanInitResult.duration()
+
+        then:
+        beanInitResult.getDuration() == beanInitResult.getActualDuration() + childBeanInitResult.getDuration()
+
+    }
+
+    def "test getTags"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        Map<String, String> tags = new HashMap<>()
+        tags.put("class", "Test")
+        beanInitResult.setTags(tags)
+
+        then:
+        beanInitResult.getTags().containsKey("class")
+
+    }
+
+    def "test getChildren"() {
+        when:
+        BeanInitResult beanInitResult = new BeanInitResult("test")
+        assertTrue(beanInitResult.getChildren().isEmpty())
+        BeanInitResult childBeanInitResult = new BeanInitResult("child")
+        beanInitResult.addChild(childBeanInitResult)
+
+        then:
+        beanInitResult.getChildren().size() == 1
+
+    }
 }
--- spring-profiler-common/src/test/java/io/github/linyimin0812/profiler/common/utils/OSUtilSpec.groovy
+++ spring-profiler-common/src/test/java/io/github/linyimin0812/profiler/common/utils/OSUtilSpec.groovy
@@ -1,6 +1,89 @@
 package io.github.linyimin0812.profiler.common.utils
 
+import com.alibaba.testable.core.tool.PrivateAccessor
+import spock.lang.Specification
+
 /**
  * @author linyimin
- **/class OSUtilSpec {
+ * */
+class OSUtilSpec extends Specification {
+
+    def "test platform"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'platform', platform)
+        def os = OSUtil.platform()
+        then:
+        os == result
+
+        where:
+        platform || result
+        OSUtil.PlatformEnum.LINUX || 'LINUX'
+        OSUtil.PlatformEnum.MACOS || 'MACOS'
+        OSUtil.PlatformEnum.WINDOWS || 'WINDOWS'
+    }
+
+    def "test isLinux"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.LINUX)
+        then:
+        OSUtil.isLinux()
+    }
+
+    def "test isMac"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.MACOS)
+        then:
+        OSUtil.isMac()
+    }
+
+    def "test isWindows"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.WINDOWS)
+        then:
+        OSUtil.isWindows()
+    }
+
+    def "test arch"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'arch', arch)
+        then:
+        OSUtil.arch() == result
+
+        where:
+        arch || result
+        'arm_32' || 'arm_32'
+        'aarch_64' || 'aarch_64'
+        'x86_32' || 'x86_32'
+        'x86_64' || 'x86_64'
+
+    }
+
+    def "test isArm32"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'arch', "arm_32")
+        then:
+        OSUtil.isArm32()
+    }
+
+    def "test isArm64"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'arch', "aarch_64")
+        then:
+        OSUtil.isArm64()
+    }
+
+    def "test isX86"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'arch', "x86_32")
+        then:
+        OSUtil.isX86()
+    }
+
+    def "test isX86_64"() {
+        when:
+        PrivateAccessor.setStatic(OSUtil, 'arch', "x86_64")
+        then:
+        OSUtil.isX86_64()
+    }
+
 }
