785c6c3cac8326540eaf5f6f70960ffd4a7c7839
linyimin0812 <linyimin520812@gmail.com>
2023-12-11 00:22:55 +0800

    test: refactor spring-profiler-extension module unit test using spock

---
+++ spring-profiler-extension/src/test/java/io/github/linyimin0812/profiler/extension/enhance/sample/AsyncProfilerListenerSpec.groovy
@@ -0,0 +1,38 @@
+package io.github.linyimin0812.profiler.extension.enhance.sample
+
+import io.github.linyimin0812.profiler.api.event.Event
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * @author linyimin
+ * */
+class AsyncProfilerListenerSpec extends Specification {
+
+    @Shared
+    AsyncProfilerListener asyncProfilerListener = new AsyncProfilerListener();
+
+    def "test filter with class name"() {
+        when:
+        def filter = asyncProfilerListener.filter("")
+
+        then:
+        !filter
+    }
+
+    def "test filter with method name and method types"() {
+        when:
+        def filter = asyncProfilerListener.filter("", new String[] {})
+
+        then:
+        filter
+    }
+
+    def "test listen"() {
+        when:
+        def List<Event.Type> list = asyncProfilerListener.listen()
+
+        then:
+        list.isEmpty()
+    }
+}
