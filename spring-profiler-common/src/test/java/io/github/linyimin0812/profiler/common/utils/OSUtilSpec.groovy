4fe36cfb13c87379e124f4e05ad6f79e818223eb
linyimin0812 <linyimin520812@gmail.com>
2023-12-11 02:40:12 +0800

    fix: remove testable-mock

--- spring-profiler-common/src/test/java/io/github/linyimin0812/profiler/common/utils/OSUtilSpec.groovy
+++ spring-profiler-common/src/test/java/io/github/linyimin0812/profiler/common/utils/OSUtilSpec.groovy
@@ -1,7 +1,8 @@
 package io.github.linyimin0812.profiler.common.utils
 
-import com.alibaba.testable.core.tool.PrivateAccessor
 import spock.lang.Specification
+
+import java.lang.reflect.Field
 
 /**
  * @author linyimin
@@ -10,7 +11,7 @@ class OSUtilSpec extends Specification {
 
     def "test platform"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'platform', platform)
+        setStatic(OSUtil, 'platform', platform)
         def os = OSUtil.platform()
         then:
         os == result
@@ -24,28 +25,28 @@ class OSUtilSpec extends Specification {
 
     def "test isLinux"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.LINUX)
+        setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.LINUX)
         then:
         OSUtil.isLinux()
     }
 
     def "test isMac"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.MACOS)
+        setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.MACOS)
         then:
         OSUtil.isMac()
     }
 
     def "test isWindows"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.WINDOWS)
+        setStatic(OSUtil, 'platform', OSUtil.PlatformEnum.WINDOWS)
         then:
         OSUtil.isWindows()
     }
 
     def "test arch"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'arch', arch)
+        setStatic(OSUtil, 'arch', arch)
         then:
         OSUtil.arch() == result
 
@@ -60,30 +61,36 @@ class OSUtilSpec extends Specification {
 
     def "test isArm32"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'arch', "arm_32")
+        setStatic(OSUtil, 'arch', "arm_32")
         then:
         OSUtil.isArm32()
     }
 
     def "test isArm64"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'arch', "aarch_64")
+        setStatic(OSUtil, 'arch', "aarch_64")
         then:
         OSUtil.isArm64()
     }
 
     def "test isX86"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'arch', "x86_32")
+        setStatic(OSUtil, 'arch', "x86_32")
         then:
         OSUtil.isX86()
     }
 
     def "test isX86_64"() {
         when:
-        PrivateAccessor.setStatic(OSUtil, 'arch', "x86_64")
+        setStatic(OSUtil, 'arch', "x86_64")
         then:
         OSUtil.isX86_64()
     }
 
+    static void setStatic(Class<?> clazz, String fieldName, Object value) {
+        Field filed = clazz.getDeclaredField(fieldName)
+        filed.accessible = true
+
+        filed.set(null, value)
+    }
 }
