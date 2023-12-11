354bfbe01f8c520b528af37a217961ba1664d82c
linyimin0812 <linyimin520812@gmail.com>
2023-12-12 00:19:57 +0800

    test: add EventDispatcherSpec

--- spring-profiler-core/src/test/java/io/github/linyimin0812/profiler/core/enhance/EventDispatcherSpec.groovy
+++ spring-profiler-core/src/test/java/io/github/linyimin0812/profiler/core/enhance/EventDispatcherSpec.groovy
@@ -1,5 +1,8 @@
 package io.github.linyimin0812.profiler.core.enhance
 
+import io.github.linyimin0812.profiler.core.container.EventListenerTest
+import io.github.linyimin0812.profiler.core.container.IocContainer
+import spock.lang.Shared
 import spock.lang.Specification
 
 /**
@@ -7,15 +10,42 @@ import spock.lang.Specification
  * */
 class EventDispatcherSpec extends Specification {
 
-    def "test atEnter"() {
+    @Shared
+    EventDispatcher eventDispatcher = new EventDispatcher()
+
+    def setupSpec() {
+        IocContainer.start()
+    }
+
+    def cleanupSpec() {
+        try {
+            IocContainer.stop()
+        } catch(Exception ignored) {
+
+        }
+    }
+
+    def "test atEnter and atExit"() {
+        when:
+        eventDispatcher.atEnter(URLClassLoader, null, 'findResource', '(Ljava/lang/String;)Ljava/net/URL;', null)
+        eventDispatcher.atExit(URLClassLoader, null, 'findResource', '(Ljava/lang/String;)Ljava/net/URL;', null, null)
+
+        then:
+        EventListenerTest.atEnterEvent != null
+        EventListenerTest.atExitEvent != null
+        EventListenerTest.atExitEvent.invokeId == EventListenerTest.atEnterEvent.invokeId
 
     }
 
-    def "test atExit"() {
+    def "test atEnter and atExceptionExit"() {
+        when:
+        eventDispatcher.atEnter(URLClassLoader, null, 'findResource', '(Ljava/lang/String;)Ljava/net/URL;', null)
+        eventDispatcher.atExceptionExit(URLClassLoader, null, 'findResource', '(Ljava/lang/String;)Ljava/net/URL;', null, null)
 
-    }
-
-    def "test atExceptionExit"() {
+        then:
+        EventListenerTest.atEnterEvent != null
+        EventListenerTest.atExceptionExitEvent != null
+        EventListenerTest.atEnterEvent.invokeId == EventListenerTest.atExceptionExitEvent.invokeId
 
     }
 }
